a02d71b343ee0df460a7ffd0342753f055a7c35c
mzoob <83409616+mzoob@users.noreply.github.com>
2024-01-03 14:17:35 +0800

    Update Spline.tsx

--- src/components/Spline/Spline.tsx
+++ src/components/Spline/Spline.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-empty-pattern */
-import { useState } from 'react';
 
 import Spline from '@splinetool/react-spline';
 
-export function App() {
+export function Spline() {
   return (
     <Spline scene="https://prod.spline.design/oNF4Mb9WsNh0q8zM/scene.splinecode" />
   );
