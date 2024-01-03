e9737ea5141c6fba5a7d883aab3d2cf741361b79
mzoob <83409616+mzoob@users.noreply.github.com>
2024-01-03 14:22:15 +0800

    Add files via upload

---
+++ src/components/MySpline/Spline.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-empty-pattern */
+
+import Spline from '@splinetool/react-spline';
+
+export function MySpline() {
+  return (
+    <Spline scene="https://prod.spline.design/oNF4Mb9WsNh0q8zM/scene.splinecode" />
+  );
+}
---
+++ src/components/MySpline/index.ts
@@ -0,0 +1,4 @@
+import { Count } from './Count';
+import { App } from './Spline';
+
+export default { App };
