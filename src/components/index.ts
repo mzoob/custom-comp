f88772a42d0b28ef6af639cfbc060e070f583ded
mzoob <83409616+mzoob@users.noreply.github.com>
2024-01-03 14:15:08 +0800

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import { HeaderMenu } from './HeaderBackground';
 import { ScrollAnimation } from './ScrollAnimation';
+import { Spline } from './Spline';
 
-export default { HeaderMenu, ScrollAnimation };
+export default { HeaderMenu, ScrollAnimation, Spline };
