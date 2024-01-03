1aa1d81dfcf027fc9d8fa272c5ae003ece61c0f2
mzoob <83409616+mzoob@users.noreply.github.com>
2024-01-03 14:16:49 +0800

    Add files via upload

---
+++ src/components/Spline/Spline.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-empty-pattern */
+import { useState } from 'react';
+
+import Spline from '@splinetool/react-spline';
+
+export function App() {
+  return (
+    <Spline scene="https://prod.spline.design/oNF4Mb9WsNh0q8zM/scene.splinecode" />
+  );
+}
