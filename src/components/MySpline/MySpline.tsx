e5ab0e3c1722ed2756cb82b10a569fdba8d8805a
mzoob <83409616+mzoob@users.noreply.github.com>
2024-01-03 14:59:56 +0800

    Update MySpline.tsx

--- src/components/MySpline/MySpline.tsx
+++ src/components/MySpline/MySpline.tsx
@@ -3,7 +3,7 @@
 
 import Spline from '@splinetool/react-spline';
 
-export default const MySpline = () => {
+export const MySpline = () => {
   return (
     <Spline scene="https://prod.spline.design/oNF4Mb9WsNh0q8zM/scene.splinecode" />
   );
